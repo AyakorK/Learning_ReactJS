a9023ad72ddd9f42c6a318430f7aca213c972186
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-02 16:26:18 +0200

    Create a page about myself

---
+++ src/pages/AboutMe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+
+const AboutMe = () => {
+    return (
+        <div>
+            <Nav />
+      
+            <h1> Coucou je suis Ayakor</h1>
+        </div>
+    );
+};
+
+export default AboutMe;
