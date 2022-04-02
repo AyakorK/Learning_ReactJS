acd9542cf7af67825a001fbba78d383322c3d63f
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-02 16:26:34 +0200

    Create a page that will stock my projects

---
+++ src/pages/MyProjects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+const MyProjects = () => {
+    return (
+        <div>
+            <Nav />
+            <h1>My Projects</h1>
+        </div>
+    );
+};
+
+export default MyProjects;
