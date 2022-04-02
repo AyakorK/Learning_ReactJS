855bbad00d8853c6a4bd993ce48c94376998feb4
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-02 19:49:43 +0200

    Update Homepage.js

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Nav from '../components/Nav';
+import Background from '../components/Background';
 
 const Homepage = () => {
     return (
-        <div>
-        <Nav />
+        <div className="homepage">
+            <Nav />
+            <Background />
             <div className="title">
-                <h1>Homepage</h1>
+                <h1>Hello !</h1>
+                <h2>I'm Guillaume MORET</h2>
+                <h3>Aka. Ayakor</h3>
+                <h3>I'm a french young developer</h3>
             </div>
         </div>
+        
     );
 };
 
