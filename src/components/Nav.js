4182308aaa1f1bf2dc61f3c99c6e6af29caaf29a
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-02 19:49:31 +0200

    Update nav to add an active css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+
+
 const Nav = () => {
     return (
-        <nav>
+        <nav className="navigation">
             <ul>
-                <NavLink to="/">
+                <NavLink to="/" className={(nav) => (nav.isActive ? "activePage": "")}>
                     <li>Homepage</li>
                 </NavLink>
-                <NavLink to="/ayakor">
+                <NavLink to="/ayakor"  className={(nav) => (nav.isActive ? "activePage": "")}>
                     <li>About Me</li>
                 </NavLink>
-                <NavLink to="/projects">
+                <NavLink to="/projects"  className={(nav) => (nav.isActive ? "activePage": "")} >
                     <li>My Projects</li>
                 </NavLink>
             </ul>
         </nav>
+        
     );
 };
 
