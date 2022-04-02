e4417ee559506c38ccd6c41380583c41e1464d78
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-02 19:49:45 +0200

    Update MyProjects.js

--- src/pages/MyProjects.js
+++ src/pages/MyProjects.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Background from '../components/Background';
 import Nav from '../components/Nav';
 
 const MyProjects = () => {
     return (
         <div>
             <Nav />
+            <Background />
             <h1>My Projects</h1>
         </div>
     );
