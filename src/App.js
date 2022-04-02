d7bbc4a66767b03b1c2fea0983d257286f120f0d
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-02 16:26:06 +0200

    Update App to start from beginning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutMe from "./pages/AboutMe";
+import Homepage from "./pages/Homepage";
+import MyProjects from "./pages/MyProjects";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter> 
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/ayakor" element={<AboutMe />} />
+        <Route path="/projects" element={<MyProjects />} />
+        <Route path="*" element={<Homepage />} />
+        
+
+      </Routes>
+     </BrowserRouter>
   );
-}
+};
 
 export default App;
