8ca3b4cf8c8bb2943797c44b4149fbbd58625a42
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-04-02 16:26:25 +0200

    Create a homepage

---
+++ src/pages/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+const Homepage = () => {
+    return (
+        <div>
+        <Nav />
+            <div className="title">
+                <h1>Homepage</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Homepage;
